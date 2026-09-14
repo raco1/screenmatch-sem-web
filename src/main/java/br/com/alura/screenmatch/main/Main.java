a2556dd0e80b25881ccea9abb988dd4bbab8c520
rafael <rafaelcoelho2711@gmail.com>
2026-09-14 11:29:49 -0300

    Aula Java: persistência de dados e consultas com Spring Data JPA. Add:Listar séries que foram buscadas durante a exec do programa.

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -14,24 +14,24 @@ import java.util.Scanner;
 public class Main {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final ConverterDados conversor = new ConverterDados();
-    private final String API_KEY = "&apikey=6585022c";
     private final Scanner scanner = new Scanner(System.in);
+    private final String API_KEY = "&apikey=6585022c";
     private final Api api = new Api();
-
-    String getEncode;
-    private final String ENCODED = URLEncoder.encode(getEncode, StandardCharsets.UTF_8);
+    private List<Dados> seriesListadas = new ArrayList<>();
 
     public void showMenu() {
-
+        var opcao = -1;
+        while (opcao != 0) {
             var menu = """
-                1 - Buscar séries
-                2 - Buscar episódios
-                
-                0 - Sair
-                """;
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar séries buscadas
+                    0 - Sair
+                    """;
 
-            System.out.println(menu);
-            int opcao = scanner.nextInt();
+            System.out.println("\n" + menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
@@ -40,36 +40,47 @@ public class Main {
                 case 2:
                     getEpisodioPorSerie();
                     break;
+                case 3:
+                    getSeriesListadas();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
                 default:
                     System.out.println("Opção inválida");
             }
-        };
-
-        private void getSerie() {
-            Dados dados = getDadosSerie();
-            System.out.println(dados);
         }
+        ;
+    }
 
-        private Dados getDadosSerie() {
-            System.out.println("Digite o nome da série para busca");
-            getEncode = scanner.nextLine();
-            var json = api.getApi(ENDERECO + ENCODED + API_KEY);
-            return conversor.obterDados(json, Dados.class);
+    private void getSerie() {
+        Dados dados = getDadosSerie();
+        seriesListadas.add(dados);
+        System.out.println(dados);
+    }
+
+    private Dados getDadosSerie() {
+        System.out.println("Digite o nome da série para busca");
+        String getEncode = scanner.nextLine();
+        String encoded = URLEncoder.encode(getEncode, StandardCharsets.UTF_8);
+        var json = api.getApi(ENDERECO + encoded + API_KEY);
+        return conversor.obterDados(json, Dados.class);
+    }
+
+    private void getEpisodioPorSerie() {
+        Dados dadosSerie = getDadosSerie();
+        List<SeasonDetail> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dadosSerie.total_temporadas(); i++) {
+            var json = api.getApi(ENDERECO + "&season=" + i + API_KEY);
+            SeasonDetail dadosTemporada = conversor.obterDados(json, SeasonDetail.class);
+            temporadas.add(dadosTemporada);
         }
+        temporadas.forEach(System.out::println);
+    }
 
-        private void getEpisodioPorSerie(){
-            Dados dadosSerie = getDadosSerie();
-            List<SeasonDetail> temporadas = new ArrayList<>();
-
-            for (int i = 1; i <= dadosSerie.total_temporadas(); i++) {
-                var json = api.getApi(ENDERECO + "&season=" + i + API_KEY);
-                SeasonDetail dadosTemporada = conversor.obterDados(json, SeasonDetail.class);
-                temporadas.add(dadosTemporada);
-            }
-            temporadas.forEach(System.out::println);
-        }
-    };
-
+    private void getSeriesListadas() {
+        System.out.println("\nSéries buscadas nessa sessão: ");
+        seriesListadas.forEach(System.out::println);
+    }
+};
